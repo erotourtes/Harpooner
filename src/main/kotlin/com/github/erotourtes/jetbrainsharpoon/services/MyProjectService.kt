31bb330467cc6c65b7bd5722a9d1d889d0a76928
GitHub Action <action@github.com>
2023-04-15 13:35:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/erotourtes/jetbrainsharpoon/services/MyProjectService.kt
+++ src/main/kotlin/com/github/erotourtes/jetbrainsharpoon/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.erotourtes.jetbrainsharpoon.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.erotourtes.jetbrainsharpoon.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
