2f09227a49b07dd1cf7b6cc0fe169416a3e49299
erotourtes <erotourtes@gmail.com>
2024-02-19 09:50:47 +0200

    Feat: added integration test for save on request lost

--- src/test/kotlin/com/github/erotourtes/harpoon/utils/menu/QuickMenuTest.kt
+++ src/test/kotlin/com/github/erotourtes/harpoon/utils/menu/QuickMenuTest.kt
@@ -1,7 +1,10 @@
 package com.github.erotourtes.harpoon.utils.menu
 
 import com.github.erotourtes.harpoon.HarpoonTestCase
+import com.github.erotourtes.harpoon.utils.TYPING_DEBOUNCE_MS
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.fileEditor.FileEditorManager
+import junit.framework.TestCase
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -59,5 +62,40 @@ class QuickMenuTest : HarpoonTestCase() {
         }
 
         Assertions.assertEquals(0, harpoonService.getPaths().size)
+    }
+
+    @Test
+    fun `should update harpooner on request lost`() {
+        fixtureAddDummyFile0()
+        performAddFileAction()
+        fixtureAddDummyFile1()
+        performAddFileAction()
+        TestCase.assertEquals(2, harpoonService.getPaths().size)
+
+        app.invokeAndWait {
+            performQuickMenuAction()
+            WriteCommandAction.runWriteCommandAction(fixture.project) {
+                menuDc.setText(
+                    """
+                $dummy1FullPath
+                $dummy0FullPath
+                """.trimIndent()
+                )
+            }
+        }
+
+        // change focus to another editor
+        app.invokeAndWait {
+            val fileEditorManager = FileEditorManager.getInstance(project)
+            val editor = fileEditorManager.allEditors.filter { it.file.name == dummy1Filename }[0]
+            fileEditorManager.openFile(editor.file, false)
+        }
+
+        // TODO: remove debouncer for testing
+        Thread.sleep(TYPING_DEBOUNCE_MS * 2)
+
+        val paths = harpoonService.getPaths()
+        TestCase.assertEquals(dummy1FullPath, paths[0])
+        TestCase.assertEquals(dummy0FullPath, paths[1])
     }
 }
