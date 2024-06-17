5ca1b46587593f57d24f1687c83378580906ffdb
Marcel Widmer <marzelwidmer@gmail.com>
2024-06-17 20:38:12 +0200

    feat: add support for move next and previous files in harpooner list (#21)

---
+++ src/main/kotlin/com/github/erotourtes/harpoon/actions/NextFileAction.kt
@@ -0,0 +1,22 @@
+package com.github.erotourtes.harpoon.actions
+
+import com.github.erotourtes.harpoon.utils.notify
+import com.github.erotourtes.harpoon.services.HarpoonService
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.service
+
+
+class NextFileAction: AnAction() {
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val project = event.project ?: return
+        val harpoonService = project.service<HarpoonService>()
+
+        try {
+            harpoonService.nextFile()
+        } catch (e: Exception) {
+            notify(e.message ?: "Error opening file")
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/erotourtes/harpoon/actions/PreviousFileAction.kt
@@ -0,0 +1,21 @@
+package com.github.erotourtes.harpoon.actions
+
+import com.github.erotourtes.harpoon.utils.notify
+import com.github.erotourtes.harpoon.services.HarpoonService
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.service
+
+ class PreviousFileAction : AnAction() {
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val project = event.project ?: return
+        val harpoonService = project.service<HarpoonService>()
+
+        try {
+            harpoonService.previousFile()
+        } catch (e: Exception) {
+            notify(e.message ?: "Error opening file")
+        }
+    }
+}
