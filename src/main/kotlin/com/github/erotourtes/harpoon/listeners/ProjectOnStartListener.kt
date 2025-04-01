2a97b08b1651d7d35df7bfe1f809d6ae1ba7292f
erotourtes <erotourtes@gmail.com>
2025-04-02 00:10:29 +0300

    fix: migrate to project activity

--- src/main/kotlin/com/github/erotourtes/harpoon/listeners/ProjectOnStartListener.kt
+++ src/main/kotlin/com/github/erotourtes/harpoon/listeners/ProjectOnStartListener.kt
@@ -2,8 +2,11 @@ package com.github.erotourtes.harpoon.listeners
 
 import com.github.erotourtes.harpoon.services.HarpoonService
 import com.github.erotourtes.harpoon.utils.*
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.CommandProcessor
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.Document
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
@@ -11,6 +14,8 @@ import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.vfs.LocalFileSystem
 
 class ProjectOnStartListener : ProjectActivity {
+    private val log = Logger.getInstance(ProjectOnStartListener::class.java)
+
     override suspend fun execute(project: Project) {
         gitIgnoreMenuFiles(project)
         initServiceIfMenuIsOpened(project)
@@ -30,25 +35,30 @@ class ProjectOnStartListener : ProjectActivity {
         val path = getGitignorePath(project) ?: return
 
         val gitignoreVF = LocalFileSystem.getInstance().findFileByPath(path) ?: return
-        val gitignoreDocument = FileDocumentManager.getInstance().getDocument(gitignoreVF) ?: return
+        val gitignoreDocument =
+            ApplicationManager.getApplication().runReadAction<Document> {
+                FileDocumentManager.getInstance().getDocument(gitignoreVF)
+            } ?: return
 
         if (gitignoreDocument.text.contains(MENU_NAME)) return
 
-        try {
-            val endLine = gitignoreDocument.getLineEndOffset(gitignoreDocument.lineCount - 1)
-            CommandProcessor.getInstance().executeCommand(
-                project, {
-                    WriteCommandAction.runWriteCommandAction(project) {
-                        val message = """
+        ApplicationManager.getApplication().invokeLater {
+            try {
+                val endLine = gitignoreDocument.getLineEndOffset(gitignoreDocument.lineCount - 1)
+                CommandProcessor.getInstance().executeCommand(
+                    project, {
+                        WriteCommandAction.runWriteCommandAction(project) {
+                            val message = """
                             # $PLUGIN_NAME
                             $MENU_NAME
                         """.trimIndent()
-                        gitignoreDocument.insertString(endLine, message)
-                    }
-                }, PLUGIN_NAME, null
-            )
-        } catch (e: Exception) {
-            println(e.toString())
+                            gitignoreDocument.insertString(endLine, message)
+                        }
+                    }, PLUGIN_NAME, null
+                )
+            } catch (e: Exception) {
+                log.error(e.toString())
+            }
         }
     }
 
