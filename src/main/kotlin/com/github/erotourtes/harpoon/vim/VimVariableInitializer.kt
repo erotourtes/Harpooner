56ce55c5bbe5c16df5f87b9dc1def0d3cd9a45d0
Max Siryk <67370189+erotourtes@users.noreply.github.com>
2025-04-05 13:07:33 +0300

    feat: set variable in a queue  (#33)
    * feat: try extension
    
    * fix: move back to projectactivity

--- src/main/kotlin/com/github/erotourtes/harpoon/vim/VimVariableInitializer.kt
+++ src/main/kotlin/com/github/erotourtes/harpoon/vim/VimVariableInitializer.kt
@@ -1,25 +1,52 @@
 package com.github.erotourtes.harpoon.vim
 
 import com.github.erotourtes.harpoon.utils.PLUGIN_NAME
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.startup.ProjectActivity
 import com.maddyhome.idea.vim.VimPlugin
+import com.maddyhome.idea.vim.newapi.vim
 import com.maddyhome.idea.vim.vimscript.model.datatypes.VimInt
+import com.maddyhome.idea.vim.vimscript.services.VimRcService.executeIdeaVimRc
 
 class VimVariableInitializer : ProjectActivity {
     private val log = Logger.getInstance(VimVariableInitializer::class.java)
 
-    private fun runUnsafe() {
-        if (!VimPlugin.isEnabled()) {
-            log.info("Vim plugin is not enabled")
-            return
+    override suspend fun execute(project: Project) {
+        ApplicationManager.getApplication().invokeLater {
+            try {
+                setGlobalVariable()
+                reloadVimRc()
+            } catch (e: Exception) {
+                log.error("Couldn't set variable", e)
+            }
         }
-        val variableService = VimPlugin.getVariableService()
-        variableService.storeGlobalVariable(PLUGIN_NAME, VimInt(1))
     }
 
-    override suspend fun execute(project: Project) {
-        runCatching(::runUnsafe).onFailure { log.error(it.message) }
+
+    private fun setGlobalVariable() {
+        log.info("Setting global variable")
+        val variableService = VimPlugin.getVariableService()
+        variableService.storeGlobalVariable(VARIABLE, VimInt(1))
+        log.info("Set global variable")
+    }
+
+    private fun reloadVimRc() {
+        log.info("Reloading .ideavimrc")
+        val projects = ProjectManager.getInstance().openProjects
+        for (project in projects) {
+            log.info("Reloading .ideavimrc for project ${project.name}")
+            val editor = FileEditorManager.getInstance(project).selectedTextEditor?.vim ?: continue
+            executeIdeaVimRc(editor)
+        }
+
+        log.info("Reloaded .ideavimrc")
+    }
+
+    companion object {
+        private const val VARIABLE = PLUGIN_NAME
     }
 }
