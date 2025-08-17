edb554ea25aa4059af33f8895096f4f3b6393e7c
Max Siryk <67370189+erotourtes@users.noreply.github.com>
2025-08-17 14:43:34 +0300

    feat: add replace entry action (#40)
    * feat: preserve entry position
    
    * feat: add replace file action
    
    * feat: update README.md
    
    * fix: next file action

---
+++ src/main/kotlin/com/github/erotourtes/harpoon/utils/State.kt
@@ -0,0 +1,157 @@
+package com.github.erotourtes.harpoon.utils
+
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+
+class State {
+    private val data: ArrayList<PathRecord?> = ArrayList()
+
+    val paths: List<String>
+        get() = data.map { it?.path ?: "" }.toList()
+
+    fun getFile(index: Int): VirtualFile? {
+        val record = data.getOrNull(index) ?: return null
+        val virtualFile = record.getVirtualFile()
+        return virtualFile
+    }
+
+    fun set(newPaths: List<String>) {
+        val seen = mutableSetOf<String>()
+        val lastIndexOfNotEmptyPath = newPaths.indexOfLast { it.isNotEmpty() }
+        val filtered = newPaths.take(lastIndexOfNotEmptyPath + 1).filter {
+            val shouldRemain = it.isEmpty() || !seen.contains(it)
+            if (shouldRemain) {
+                seen.add(it)
+            }
+            return@filter shouldRemain
+        }.map { PathRecord.from(it) }
+        data.clear()
+        data.addAll(filtered)
+    }
+
+    fun add(path: String) {
+        if (includes(path) || path.isEmpty()) return
+        val record = PathRecord.from(path)
+        val firstEmptyEntryIndex = data.indexOfFirst { it == null }
+        when {
+            firstEmptyEntryIndex != -1 -> data[firstEmptyEntryIndex] = record
+            else -> data.add(record)
+        }
+    }
+
+    fun remove(path: String) {
+        val record = data.find { it?.isTheSameAs(path) ?: false }
+        if (record == null) return
+        data.remove(record)
+    }
+
+    fun clear() {
+        data.clear()
+    }
+
+    fun replace(atIndex: Int, withPath: String) {
+        if (atIndex < 0 || withPath.isEmpty()) {
+            return
+        }
+
+        data.replaceAll {
+            if (it?.isTheSameAs(withPath) == true) null else it
+        }
+
+        val record = PathRecord.from(withPath) ?: return
+        while (data.size <= atIndex) {
+            data.add(null)
+        }
+        data[atIndex] = record
+
+        while (data.isNotEmpty() && data.last() == null) {
+            data.removeLast()
+        }
+    }
+
+    fun getNextIndexOf(path: String?): Int {
+        val index = getRelativeIndexOf(path, forward = true)
+        return index
+    }
+
+    fun getPrevIndexOf(path: String?): Int {
+        val index = getRelativeIndexOf(path, forward = false)
+        return index
+    }
+
+    private fun getRelativeIndexOf(path: String?, forward: Boolean): Int {
+        val index = getIndexOf(path).let {
+            when {
+                it != -1 -> it
+                data.size == 0 -> -1
+                forward -> -1
+                else -> data.size
+            }
+        }
+
+        for (step in 1..data.size) {
+            val offset = if (forward) step else -step
+            val nextIndex = (index + offset + data.size) % data.size
+            if (data[nextIndex] != null) {
+                return nextIndex
+            }
+        }
+
+        return index
+    }
+
+    private fun getIndexOf(path: String?): Int {
+        if (path.isNullOrEmpty()) {
+            return -1
+        }
+        val index = data.indexOfFirst { it?.isTheSameAs(path) ?: false }
+        return index
+    }
+
+    fun includes(path: String): Boolean = getIndexOf(path) != -1
+
+    fun update(oldPath: String, newPath: String): Boolean {
+        val oldRecordIndex = data.indexOfFirst { it?.isTheSameAs(oldPath) ?: false }
+        if (oldRecordIndex == -1) {
+            return false
+        }
+
+        data[oldRecordIndex] = PathRecord.from(newPath)
+        return true
+    }
+
+    fun size(): Int = data.size
+
+    private class PathRecord private constructor(val path: String) {
+        private val _virtualFile: VirtualFile? by lazy { findVirtualFile(path) }
+
+        fun getVirtualFile(): VirtualFile? {
+            val virtualFile = this._virtualFile
+            if (virtualFile == null || !virtualFile.isValid) {
+                return null
+            }
+            return virtualFile
+        }
+
+        fun isTheSameAs(other: String): Boolean {
+            // TODO: don't compare by raw paths
+            return this.path == other
+        }
+
+        companion object {
+            fun from(path: String): PathRecord? {
+                if (path.isEmpty()) {
+                    return null
+                }
+
+                return PathRecord(path)
+            }
+        }
+    }
+}
+
+private fun findVirtualFile(path: String): VirtualFile? {
+    val file = LocalFileSystem.getInstance().findFileByPath(path)
+    return file
+}
+
---
+++ src/test/kotlin/com/github/erotourtes/harpoon/utils/StateTest.kt
@@ -0,0 +1,358 @@
+package com.github.erotourtes.harpoon.utils
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class StateTest {
+    @Test
+    fun `should set all the paths`() {
+        val state = State()
+        state.set(
+            listOf(
+                "",
+                "",
+                "file2.txt",
+                "",
+                "file4.txt",
+                "file5.txt",
+                "",
+                "",
+            )
+        )
+
+        assertArrayEquals(
+            arrayOf("", "", "file2.txt", "", "file4.txt", "file5.txt"),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should include the right files and not include empty files`() {
+        val state = State()
+        state.set(
+            listOf(
+                "file0.txt",
+                "",
+                "file2.txt",
+                "file3.txt"
+            )
+        )
+
+        assertTrue(state.includes("file0.txt"))
+        assertFalse(state.includes(""))
+        assertFalse(state.includes("non-existing.txt"))
+    }
+
+    @Test
+    fun `should append entry`() {
+        val state = State()
+
+        state.add("file0.txt")
+
+        assertArrayEquals(
+            arrayOf("file0.txt"),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should not append empty entry`() {
+        val state = State()
+
+        state.add("")
+
+        assertArrayEquals(
+            arrayOf(),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should replace first empty entry`() {
+        val state = State()
+        state.set(
+            listOf(
+                "file0.txt",
+                "",
+                "file2.txt",
+                "file3.txt"
+            )
+        )
+
+        state.add("file4.txt")
+
+        assertArrayEquals(
+            arrayOf("file0.txt", "file4.txt", "file2.txt", "file3.txt"),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should not append existing entry`() {
+        val state = State()
+
+        state.add("file0.txt")
+        state.add("file0.txt")
+
+        assertArrayEquals(
+            arrayOf("file0.txt"),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should remove entry`() {
+        val state = State()
+        state.set(
+            listOf(
+                "file0.txt",
+                "",
+                "file2.txt",
+                "file3.txt"
+            )
+        )
+
+        state.remove("file2.txt")
+
+        assertArrayEquals(
+            arrayOf("file0.txt", "", "file3.txt"),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should not remove empty entries`() {
+        val state = State()
+        state.set(
+            listOf(
+                "file0.txt",
+                "",
+                "file2.txt",
+                "file3.txt"
+            )
+        )
+
+        state.remove("")
+
+        assertEquals(4, state.paths.size)
+        assertEquals("", state.paths[1])
+    }
+
+    @Test
+    fun `should clear all files`() {
+        val state = State()
+        state.set(
+            listOf(
+                "file0.txt",
+                "",
+                "file2.txt",
+                "file3.txt"
+            )
+        )
+        assertFalse(state.paths.isEmpty())
+
+        state.clear()
+
+        assertTrue(state.paths.isEmpty())
+    }
+
+    @Test
+    fun `should update existing entry`() {
+        val state = State()
+        state.set(
+            listOf(
+                "file0.txt",
+                "",
+                "file2.txt",
+                "file3.txt"
+            )
+        )
+
+        val modified = state.update("file2.txt", "file-replaced.txt")
+
+        assertTrue(modified)
+        assertArrayEquals(
+            arrayOf("file0.txt", "", "file-replaced.txt", "file3.txt"),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should not update non-existing entry`() {
+        val state = State()
+        state.set(
+            listOf(
+                "file0.txt",
+                "",
+                "file2.txt",
+                "file3.txt"
+            )
+        )
+
+        val modified = state.update("file-non-existing.txt", "file-replaced.txt")
+
+        assertFalse(modified)
+        assertArrayEquals(
+            arrayOf("file0.txt", "", "file2.txt", "file3.txt"),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should replace existing entries`() {
+        val state = State()
+        state.set(
+            listOf(
+                "file0.txt",
+                "",
+                "file2.txt",
+                "file3.txt"
+            )
+        )
+
+        state.replace(0, "file0r.txt")
+        assertArrayEquals(
+            arrayOf("file0r.txt", "", "file2.txt", "file3.txt"),
+            state.paths.toTypedArray()
+        )
+
+        state.replace(1, "file1r.txt")
+        assertArrayEquals(
+            arrayOf("file0r.txt", "file1r.txt", "file2.txt", "file3.txt"),
+            state.paths.toTypedArray()
+        )
+
+        state.replace(1, "file1rr.txt")
+        assertArrayEquals(
+            arrayOf("file0r.txt", "file1rr.txt", "file2.txt", "file3.txt"),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should not replace entries`() {
+        val state = State()
+        val originalList = listOf(
+            "file0.txt",
+            "",
+            "file2.txt",
+            "file3.txt"
+        )
+        state.set(originalList)
+
+        state.replace(-1, "file0r.txt")
+        assertArrayEquals(
+            originalList.toTypedArray(),
+            state.paths.toTypedArray()
+        )
+
+        state.replace(0, "")
+        assertArrayEquals(
+            originalList.toTypedArray(),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should replace entry at non existing index`() {
+        val state = State()
+        val originalList = listOf(
+            "file0.txt",
+            "",
+            "file2.txt",
+            "file3.txt"
+        )
+        state.set(originalList)
+
+        state.replace(5, "file5.txt")
+
+        assertArrayEquals(
+            arrayOf("file0.txt", "", "file2.txt", "file3.txt", "", "file5.txt"),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should replace existing entry`() {
+        val state = State()
+        val originalList = listOf(
+            "file0.txt",
+            "",
+            "file2.txt",
+            "file3.txt"
+        )
+        state.set(originalList)
+
+        state.replace(1, "file2.txt")
+        assertArrayEquals(
+            arrayOf("file0.txt", "file2.txt", "", "file3.txt"),
+            state.paths.toTypedArray()
+        )
+
+        state.replace(0, "file3.txt")
+        assertArrayEquals(
+            arrayOf("file3.txt", "file2.txt"),
+            state.paths.toTypedArray()
+        )
+    }
+
+    @Test
+    fun `should return next indexes of existing entries`() {
+        val state = State()
+        state.set(
+            listOf(
+                "",
+                "file1.txt",
+                "",
+                "file3.txt",
+                "file4.txt",
+                "",
+                "",
+                "file7.txt"
+            )
+        )
+
+        assertEquals(3, state.getNextIndexOf("file1.txt"))
+        assertEquals(4, state.getNextIndexOf("file3.txt"))
+        assertEquals(7, state.getNextIndexOf("file4.txt"))
+        assertEquals(1, state.getNextIndexOf("file7.txt"))
+        assertEquals(1, state.getNextIndexOf("non-existing.txt"))
+    }
+
+    @Test
+    fun `should return -1 for next index in empty paths`() {
+        val state = State()
+
+        assertEquals(-1, state.getNextIndexOf("non-existing.txt"))
+    }
+
+    @Test
+    fun `should return prev indexes of existing entries`() {
+        val state = State()
+        state.set(
+            listOf(
+                "",
+                "file1.txt",
+                "",
+                "file3.txt",
+                "file4.txt",
+                "",
+                "",
+                "file7.txt"
+            )
+        )
+
+        assertEquals(7, state.getPrevIndexOf("file1.txt"))
+        assertEquals(1, state.getPrevIndexOf("file3.txt"))
+        assertEquals(3, state.getPrevIndexOf("file4.txt"))
+        assertEquals(4, state.getPrevIndexOf("file7.txt"))
+        assertEquals(7, state.getPrevIndexOf("non-existing.txt"))
+    }
+
+    @Test
+    fun `should return -1 for prev index in empty paths`() {
+        val state = State()
+
+        assertEquals(-1, state.getPrevIndexOf("non-existing.txt"))
+    }
+}
