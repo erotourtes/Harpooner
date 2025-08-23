b88cac69542a103c90859ba7579f137a7098cd40
erotourtes <erotourtes@gmail.com>
2025-08-23 10:54:13 +0300

    feat: intellij platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -157,26 +157,26 @@ tasks {
     }
 }
 
-//intellijPlatformTesting {
-//    runIde {
-//        register("runIdeForUiTests") {
-//            task {
-//                jvmArgumentProviders += CommandLineArgumentProvider {
-//                    listOf(
-//                        "-Drobot-server.port=8082",
-//                        "-Dide.mac.message.dialogs.as.sheets=false",
-//                        "-Djb.privacy.policy.text=<!--999.999-->",
-//                        "-Djb.consents.confirmation.enabled=false",
-//                    )
-//                }
-//            }
-//
-//            plugins {
-//                robotServerPlugin()
-//            }
-//        }
-//    }
-//}
+intellijPlatformTesting {
+    runIde {
+        register("runIdeForUiTests") {
+            task {
+                jvmArgumentProviders += CommandLineArgumentProvider {
+                    listOf(
+                        "-Drobot-server.port=8082",
+                        "-Dide.mac.message.dialogs.as.sheets=false",
+                        "-Djb.privacy.policy.text=<!--999.999-->",
+                        "-Djb.consents.confirmation.enabled=false",
+                    )
+                }
+            }
+
+            plugins {
+                robotServerPlugin()
+            }
+        }
+    }
+}
 
 idea {
     module {
