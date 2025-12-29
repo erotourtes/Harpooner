398f95339a88fcd89e0ce03643eccb533ba50036
erotourtes <erotourtes@gmail.com>
2025-12-29 18:13:35 +0100

    feat: run integration tests

---
+++ src/intTest/kotlin/com/github/erotourtes/harpoon/actions/AddFileActionTest.kt
@@ -0,0 +1,10 @@
+package com.github.erotourtes.harpoon.actions
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+class AddFileActionTest : BasePlatformTestCase() {
+    fun testAddFileAction() {
+        print("passed")
+        assertNotNull(null)
+    }
+}
